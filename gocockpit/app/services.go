45392df89bd42c1cfaa3fd444f0e9a1b3e1cac01
Lukas Winterhalter <lwinterhalter@gmx.ch>
2016-11-30 13:21:38 +0100

    basic service list view implemented

---
+++ gocockpit/app/services.go
@@ -0,0 +1,48 @@
+package app
+
+import (
+	"fmt"
+	"time"
+	"sync"
+)
+
+type Service struct {
+	Id 		string `json:"id"`
+	Status	string `json:"status"`
+	Timestamp time.Time `json:"timestamp"`
+	StatusUrl	string `json:"status_url"`
+	Data	map[string]string `json:"data"`
+}
+
+var services = make(map[string]Service)
+
+func init() {
+	RegisterService("example.service");
+}
+
+func GetAllServices() []Service {
+
+	list := make([]Service, 0, len(services))
+
+	for _, service := range services {
+		list = append(list, service)
+	}
+
+	return list
+}
+
+func RegisterService(id string) Service {
+
+	service, exists := services[id]
+
+	if(!exists) {
+		service = Service{}
+		service.Id = "luki"
+		service.Status = "UP"
+		service.StatusUrl = "http://some/endpoint"
+		service.Timestamp = time.Now();
+		services[id] = service
+	}
+
+	return service
+}
