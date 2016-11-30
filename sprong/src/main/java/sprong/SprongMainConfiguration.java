9c7bcf38317b687ec1ec068af91c68724e06ca4b
Lukas Winterhalter <lwinterhalter@gmx.ch>
2016-11-30 09:27:12 +0100

    sprong service added

---
+++ sprong/src/main/java/sprong/SprongMainConfiguration.java
@@ -0,0 +1,13 @@
+package sprong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprongMainConfiguration {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SprongMainConfiguration.class, args);
+    }
+
+}
